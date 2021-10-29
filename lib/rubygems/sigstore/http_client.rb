56bb280e0ef8a91762192e99049fa2279e0d8afb
rochlefebvre <roch.lefebvre@shopify.com>
2021-10-29 09:31:45 -0400

    Restore default SSL peer verification for connections to fulcio

--- lib/rubygems/sigstore/http_client.rb
+++ lib/rubygems/sigstore/http_client.rb
@@ -18,8 +18,7 @@ require "openssl"
 class HttpClient
     def initialize; end
     def get_cert(id_token, proof, pub_key, fulcio_host)
-        # rekor uses a self signed certificate which failes the ssl check
-        connection = Faraday.new(ssl: { verify: false }) do |request|
+        connection = Faraday.new do |request|
             request.authorization :Bearer, id_token.to_s
             request.url_prefix = fulcio_host
             request.request :json
@@ -30,7 +29,7 @@ class HttpClient
         return fulcio_response.body
     end
     def submit_rekor(pub_key, data_digest, data_signature, certPEM, data_raw, rekor_host)
-        # rekor uses a self signed certificate which failes the ssl check
+        # rekor uses a self signed certificate which fails the ssl check
         connection = Faraday.new(ssl: { verify: false }) do |request|
             # request.authorization :Bearer, id_token.to_s
             request.url_prefix = rekor_host
