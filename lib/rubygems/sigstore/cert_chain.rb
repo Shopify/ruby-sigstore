d9fe3e24b75e5c038d8819372fcd5f0b04bcde47
rochlefebvre <roch.lefebvre@shopify.com>
2021-11-01 09:03:36 -0400

    define CertChain and CertExtensions

---
+++ lib/rubygems/sigstore/cert_chain.rb
@@ -0,0 +1,51 @@
+require "open-uri"
+require "rubygems/sigstore/cert_extensions"
+
+class Gem::Sigstore::CertChain
+  PATTERN = /-----BEGIN CERTIFICATE-----(?:.|\n)+?-----END CERTIFICATE-----/.freeze
+
+  def initialize(cert_pem)
+    @cert_pem = cert_pem
+  end
+
+  def certificates
+    @certificates ||= build_chain
+  end
+
+  def signing_cert
+    certificates.last
+  end
+
+  def root_cert
+    certificates.first
+  end
+
+  private
+
+  def build_chain
+    deserialize.tap do |chain|
+      while chain.first&.extension("authorityInfoAccess") do
+        chain.prepend(retrieve_issuer_cert(chain.first))
+      end
+    end
+  end
+
+  def deserialize
+    return [] unless @cert_pem
+    @cert_pem.scan(PATTERN).map do |cert|
+      cert = OpenSSL::X509::Certificate.new(cert)
+      cert.extend(Gem::Sigstore::CertExtensions)
+      cert
+    end
+  end
+
+  def retrieve_issuer_cert(cert)
+    issuer_cert_url = cert.authority_info_access.match(/http\S+/).to_s
+    raise "unsupported authorityInfoAccess value #{cert.authority_info_access}" if issuer_cert_url.empty?
+
+    cert_pem = URI.open(issuer_cert_url).read
+    issuer = OpenSSL::X509::Certificate.new(cert_pem)
+    issuer.extend(Gem::Sigstore::CertExtensions)
+    issuer
+  end
+end
